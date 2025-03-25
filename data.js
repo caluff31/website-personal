f91a40963b6b2da4e62f3f7facce01756c8ab864
caiii <lffydmnkt@gmail.com>
2025-03-25 20:07:45 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,56 @@
+let songs = [
+    {
+        name: 'About You',
+        path: 'The 1975 - About You (Lyrics).m4a',
+        artist: 'The 1975',
+        cover: 'aboutyou.jpg',
+    },
+    {
+        name: 'Let Me Love You',
+        path: 'DJ Snake - Let Me Love You.mp3',
+        artist: 'Justin Bieber',
+        cover: '8598ed08ed941db94f7f17208149283e.jpg',
+    },
+    {
+        name: 'Ghost',
+        path: 'Indolirik - Justin Bieber  Ghost  Lirik Terjemahan.mp3',
+        artist: 'Justin Bieber',
+        cover: 'b6665fbc6d8f83459d57dcf1b1e55df2.jpg',
+    },
+    {
+        name: 'With A Wish',
+        path: 'Toilet-bound Hanako-kun Season 2 - Endin.m4a',
+        artist: 'Akari Kito',
+        cover: 'tbhk.jpg',
+    },
+    {
+        name: 'Last Night on Earth',
+        path: 'Green Day - Last Night on Earth (Lyrics).m4a',
+        artist: 'Green Day',
+        cover: '3c5f5ccab1825d270129dce586a85752.jpg',
+    },
+    {
+        name: 'Say You Wont Let Go',
+        path: 'James Arthur - Say You Won t Let Go (Lyr.m4a',
+        artist: 'James Arthur',
+        cover: 'um gatito nerdola ☝️🤓.jpeg',
+    },
+    {
+        name: 'Thank You For Everything',
+        path: 'Diego Gonzalez   Thank You For Everythin.m4a',
+        artist: 'Diego Gonzalez',
+        cover: 'love letter for u!.jpeg',
+    },
+    {
+        name: 'Labyrinth',
+        path: 'taylor swift - labyrinth (sped up   reve.m4a',
+        artist: 'taylor swift',
+        cover: 'Womina.jpeg',
+    },
+    {
+        name: 'Give Me Your Forever',
+        path: 'Zack Tabudlo - Give Me Your Forever (Lyr.m4a',
+        artist: 'Zack Tabudlo',
+        cover: '65a327336f2c541fff831cfc1d2d3dd0.jpg',
+    },
+]
